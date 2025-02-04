1b0f7aafe798f4e7eac94c80ef9bcbe7f3cc5d21
Yuji Sato <u.ji.jp777@gmail.com>
2025-02-04 09:51:19 +0100

    fix: styling + bg image for auth screens

---
+++ lib/src/constants/image_constants/bg_image_constants.dart
@@ -0,0 +1,4 @@
+const String bgCircleTopLeft =
+    "assets/images/background/bg_circle_top_left.png";
+const String bgCircleBottomRight =
+    "assets/images/background/bg_circle_bottom_right.png";
--- lib/src/widgets/scaffold/auth_scaffold.dart
+++ lib/src/widgets/scaffold/auth_scaffold.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:migla_flutter/src/constants/image_constants/bg_image_constants.dart';
 import 'package:migla_flutter/src/theme/theme_constants.dart';
 
 class AuthScaffold extends StatelessWidget {
@@ -9,12 +10,28 @@ class AuthScaffold extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgPrimaryColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 48.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: children,
-        ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(bgCircleTopLeft),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Image.asset(bgCircleBottomRight),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 24.0, vertical: 48.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: children,
+            ),
+          ),
+        ],
       ),
     );
   }
