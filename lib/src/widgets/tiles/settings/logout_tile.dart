b749ea1880655c6170fb4206dc54c6797bb6e3ad
Yuji Sato <u.ji.jp777@gmail.com>
2025-03-04 22:25:19 +0100

    feat(logout): Implement logout functionality with navigation to login screen

--- lib/src/widgets/tiles/settings/logout_tile.dart
+++ lib/src/widgets/tiles/settings/logout_tile.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:migla_flutter/src/constants/image_constants/svg_icon_constants.dart';
 import 'package:migla_flutter/src/extensions/localization/localization_context_extension.dart';
+import 'package:migla_flutter/src/screens/auth/login_screen.dart';
+import 'package:migla_flutter/src/screens/auth/register_screen.dart';
 import 'package:migla_flutter/src/theme/theme_constants.dart';
 import 'package:nb_utils/nb_utils.dart';
 
@@ -26,6 +28,9 @@ class LogoutTile extends StatelessWidget {
             fontWeight: FontWeight.bold,
             color: errorColor,
           )),
+      onTap: () {
+        LoginScreen().launch(context, isNewTask: true);
+      },
     );
   }
 }
