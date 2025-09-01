467ddd5e46966114d51c3077c5ec90956238c893
Yuji Sato <u.ji.jp777@gmail.com>
2025-09-01 16:32:31 +0200

    feat: show red badge on notification action button.

--- lib/src/widgets/buttons/notification_appbar_action_button.dart
+++ lib/src/widgets/buttons/notification_appbar_action_button.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:migla_flutter/src/models/api/notification/notification_query.dart';
 import 'package:migla_flutter/src/screens/dashboard/notification_screens/notification_list_screen.dart';
 
 class NotificationAppbarActionButton extends StatelessWidget {
@@ -6,16 +10,48 @@ class NotificationAppbarActionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-        onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const NotificationListScreen()));
+    return Query(
+      options: QueryOptions(
+        document: gql(notificationListQuery),
+        variables: {
+          'locale': 'ja', // anything is okay here. just to show the red tip.
         },
-        icon: const Icon(
-          Icons.notifications,
-          size: 35,
-        ));
+      ),
+      builder: (result, {fetchMore, refetch}) {
+        bool hasUnread =
+            (result.data?['Notifications']['docs'] as List).any((notification) {
+          return (notification['readRecords']['docs'] as List).isEmpty;
+        });
+        return IconButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const NotificationListScreen()));
+          },
+          icon: Stack(
+            alignment: Alignment.center,
+            children: [
+              const Icon(
+                Icons.notifications,
+                size: 35,
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  width: 7,
+                  height: 7,
+                  margin: EdgeInsets.only(left: 15, top: 8),
+                  decoration: BoxDecoration(
+                    color: hasUnread ? Colors.red : Colors.transparent,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
